b8dcc2ca47cd3fd0303f8011f867d76a25876d1d
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-04-01 16:45:25 -0500

    Custom Types ("interfaces")

---
+++ src/customTypes.js
@@ -0,0 +1,28 @@
+/**
+ * New user object ("interface")
+ * @typedef {object} User
+ * @property {number} id User id
+ * @property {string} name User's name
+ * @property {number|string} [age] User's age (Optional)
+ * @property {boolean} isActive User state
+ */
+
+/**
+ * @type {User}
+ */
+const newUser = {
+  age: 20,
+  id: 90,
+  isActive: false,
+  name: 'Brandon'
+}
+
+/**
+ * @type {User}
+ */
+const newUser2 = {
+  age: 20,
+  id: 90,
+  isActive: false,
+  name: 'Brandon'
+}
