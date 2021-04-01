ec46a5d30b21fba000df580d69f3ddf9c31fb88e
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-04-01 16:29:42 -0500

    Functions

---
+++ src/functions.js
@@ -0,0 +1,17 @@
+/**
+ * Add Two numbers
+ * @param {number} n1 First number
+ * @param {number} n2 Second number
+ * @returns {number} Total
+ */
+function add (n1, n2) {
+  return n1 + n2
+}
+
+/**
+ * Add two numbers and returns a string
+ * @param {number} n1 First Number to plus
+ * @param {number} n2 Second Number to plus
+ * @returns {string} Total
+ */
+const add2 = (n1, n2) => `The result(${n1} + ${n2}): ${n1 + n2}`
