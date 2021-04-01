e34b3ce1684de6b8adebe26559a48bad84bd1747
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-04-01 16:05:02 -0500

    documenting arrays

---
+++ src/arrays.js
@@ -0,0 +1,26 @@
+/**
+ * Array of numbers
+ * @type {Array}
+ */
+
+const numbers = [1, true, [], 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+/**
+ * Array Of User's Scores
+ * @type {Array<number>}
+ */
+const scores = [300, 500, 100]
+
+/**
+ * Array Of numbers and strings
+ * @type {Array<number|string>}
+ */
+const scoresAndStrings = [300, 500, 100, "2000"]
+
+/**
+ * Array Of numbers and strings
+ * @type {Array<string>}
+ */
+const strings = ["text"]
+strings.push('text 2') // Argument of type 'number' is not assignable to parameter of type 'string'.ts(2345)
+
