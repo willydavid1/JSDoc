e0197657f2ad0ae6e124f29575707db740384acb
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-04-01 16:18:01 -0500

    Objects

---
+++ src/objects.js
@@ -0,0 +1,23 @@
+/**
+ * Person Object
+ * @type {{ id: number, firstName: string, lastName: string, age: number }}
+ */
+
+const person = {
+  id: 90,
+  firstName: "Brandon",
+  lastName: "Scott",
+  age: 20
+}
+
+/**
+ * Person Object two
+ * @type {{ id: number|string, firstName: string, lastName: string, age: number }}
+ */
+
+const person2 = {
+  id: 1,
+  firstName: "Brandon",
+  lastName: "Scott",
+  age: 30
+}
