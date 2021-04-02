7e65eb41e28a3e045c351a16ff9c75be9f36689a
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-04-01 19:01:04 -0500

    Classes | @see | @todo | Using typedef from another site

---
+++ src/classes.js
@@ -0,0 +1,35 @@
+/**
+ * Class to create a Programmer
+ * 
+ * @example
+ * const newProgramer = new Programmer({ name: 'Brandon', id: 3, isActive: true }, 'javascript')
+ * newProgramer.getInfo() // I'm ---Brandon--- and my favorite programming language is ---javascript---
+ * 
+ * @see https://jsdoc.app/howto-es2015-classes.html
+ * @todo Implement the rest of methods
+ */
+
+class Programmer {
+  /**
+   * @param {User} user User's Information
+   * @param {string} language A programming language
+   */
+  constructor (user, language) {
+    this.fullName = user.name
+    this.language = language
+  }
+
+  /**
+   * Get programmer information
+   * @returns {void}
+   */
+  getInfo () {
+    console.log(`I'm ${this.fullName} and my favorite programming language is ${this.language}`)
+  }
+}
+
+const programmerOne = new Programmer({ name: 'Brandon', id: 3, isActive: true }, 'javascript')
+const programmerTwo = new Programmer({ name: 'Brandon', id: 3, isActive: true }, 'go')
+
+programmerOne.getInfo()
+programmerTwo.getInfo()
