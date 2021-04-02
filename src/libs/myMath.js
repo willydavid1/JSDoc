b1252593012a3214a0d075737775afde4ff4bf98
Willy Da Conceicao <willydavidlozada@gmail.com>
2021-04-01 19:23:27 -0500

    @module

---
+++ src/libs/myMath.js
@@ -0,0 +1,25 @@
+/**
+ * My Math library
+ * @module MyMath
+ */
+
+/**
+ * Add two numbers
+ * @param {number} x First Number
+ * @param {number} y Second Number
+ * @returns {number} Sum of X and Y
+ */
+const add = (x, y) => x + y
+
+/**
+ * Substract Two numbers
+ * @param {number} x First Number
+ * @param {number} y Second Number
+ * @returns {number} substract of X and Y
+ */
+const substract = (x, y) => x - y
+
+module.exports = {
+  add,
+  substract
+}
